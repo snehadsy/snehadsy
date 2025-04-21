98f700fd3fdf44b78e703e214b44ecae0451de62
sayliraut <sayliraut2903@gmail.com>
2025-04-21 17:17:30 +0530

    add and list students

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\studentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/students', [studentController::class, 'index'])->name('students.index');
+Route::get('/students/add', [studentController::class, 'add'])->name('students.add');
+Route::get('/students/{id}', [StudentController::class, 'show'])->name('students.show');
+Route::delete('/students/{id}', [StudentController::class, 'destroy'])->name('students.destroy');
+Route::get('/students/{id}/export', [StudentController::class, 'export'])->name('students.export');
+Route::post('/students/store', [StudentController::class, 'store'])->name('students.store');
+
+
