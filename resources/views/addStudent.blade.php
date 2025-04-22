0b6306c5009c29b91e3a2cab0a2ccd041aabac6b
sayliraut <sayliraut2903@gmail.com>
2025-04-22 13:03:53 +0530

    change

--- resources/views/addStudent.blade.php
+++ resources/views/addStudent.blade.php
@@ -14,7 +14,7 @@
 
 <body>
 
-    <div class="main_sec py-5">
+    <div class=" py-5">
         <div class="container">
             <h1 class="text-center mb-5">Add Student</h1>
             <a href="{{ route('students.index') }}" class="btn btn-secondary mt-3" >Back to List</a>
